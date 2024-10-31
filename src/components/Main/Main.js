a53294346d51cad88a24c42dc46bce686f5482ec
clintonOuko <oukoclin@gmail.com>
2024-10-31 22:44:21 +0300

    first commit

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './main.css'
+
+function Main() {
+  return (
+    <div>
+        <h1>Feedlify Organization </h1>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/feedback/FeedbackForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeedbackForm() {
+  return (
+    <div>
+        <h1>User Feedback form</h1>
+    </div>
+  )
+}
+
+export default FeedbackForm
